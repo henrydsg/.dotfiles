ca2e0e469ee0cde3c119dcadf3cdc9e710cfce24
Yihung Yu <henry840227@gmail.com>
2022-06-25 22:26:40 -0400

    Add <space>t for fzf :Files.

--- .config/nvim/lua/user/fzf.lua
+++ .config/nvim/lua/user/fzf.lua
@@ -1,2 +1,8 @@
 vim.cmd [[let $FZF_DEFAULT_OPTS="--preview 'bat --color=always {}'"]]
 vim.cmd [[let $BAT_THEME="Dracula"]]
+
+local opts = { noremap = true, silent = true }
+
+local keymap = vim.keymap.set
+
+keymap("n", "<Space>t", ":Files<CR>", opts)
