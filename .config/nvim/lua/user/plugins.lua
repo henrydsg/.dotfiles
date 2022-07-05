6b6749e8ea75df70a135e275c281ca410abf685f
Yihung Yu <henry840227@gmail.com>
2022-07-05 10:45:02 -0400

    Fix nvim fzf install.

--- .config/nvim/lua/user/plugins.lua
+++ .config/nvim/lua/user/plugins.lua
@@ -88,8 +88,10 @@ return packer.startup(function(use)
   use({
     "junegunn/fzf.vim",
     requires = {
-      "junegunn/fzf",
-      run = ":call fzf#install()",
+      {
+        "junegunn/fzf",
+        run = ":call fzf#install()",
+      }
     },
     config = "require 'user.fzf'"
   })
