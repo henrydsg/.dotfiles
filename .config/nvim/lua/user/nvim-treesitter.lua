dc61ac2a65190440ed84a650a2f0e9605ba2870e
Yihung Yu <henry840227@gmail.com>
2022-07-07 15:19:30 -0400

    Turn off nvim-treesitter indent function.
    It is still in experiment and seems not good enough.

--- .config/nvim/lua/user/nvim-treesitter.lua
+++ .config/nvim/lua/user/nvim-treesitter.lua
@@ -7,7 +7,7 @@ configs.setup({
   ensure_installed = "all",
   ignore_install = { "phpdoc" }, -- It seems that a bug pops up installing phpdoc.
   sync_install = false,
-  indent = { enable = true, disable = { "yaml" } },
+  indent = { enable = false, disable = { "yaml" } },
   highlight = {
     enable = true,
     additional_vim_regex_highlighting = false,
